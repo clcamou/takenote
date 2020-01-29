e9a8f56b818e8f9311859ec5e5979d93167c4c37
clcamou <clcamou@gmail.com>
2020-01-29 14:42:47 -0700

    tried to fix the linking issue

--- server.js
+++ server.js
@@ -1,79 +1,68 @@
 //dependencies 
-const fs = require("fs")
-const express = require("express")
-const path = require("path")
+const fs = require("fs");
+const express = require("express");
+
+//html route 
+const path = require("path");
 
 let app = express();
 
 //set port
 let PORT = process.env.PORT || 8181; 
 
-//use file system to write to the database
-fs.writeFile(path.join(__dirname, "db/db.json"), "[]",  function (err) {
-    if (err) {
-        return console.log(err);
-    } 
-    console.log("Success")
-})
 
+
+//set server to use express 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+//use public 
 app.use(express.static("public"));
+
+//use file system to write to the database
+app.get("/api/notes", function(req, res){
+    let dbParsed;
+    fs.readFile("./db/db.JSON", function(err, data){
+        if (err) {
+            console.log(err)
+        } else {
+            return res.json(JSON.parse(data))
+        }
+    })
+});
+
+//Send data to the database 
+app.post("/api/notes", function(res, req){
+    let newNote = req.body 
+    console.log(newNote)
+    let dataTable;
+
+    fs.readFile("./db/db.JSON", function(err, data){
+        if (err) {
+            console.log(err)
+        } else {
+            dataTable = JSON.parse(data)
+            dataTable.push(newNote)
+            console.log("Success!")
+        }
+    })
+})
 
 //GET data from database 
 
-app.get ("/notes", function(req, res){
-    res.json(path.join(__dirname, "public/index.html"));
-})
-
-app.get("/notes", function(req, res){
-    res.sendfile(path.join(__dirname, "public/notes.html"))
+app.get ("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/notes.html"));
 });
 
-app.get("/api/notes", function (req, res) {
-    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function (err, data) {
-        if (err) {
-            return console.log(err);
-        } 
-        data = JSON.parse(data)
-        res.send(data);
-        })
+app.get("/notes", function(req, res){ 
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.post("/api/notes", function(req, res) {
-    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data) {
-        if (err) {
-            return console.log(err);
-        }; 
-        data = JSON.parse(data)
-        data.push(req.body)
-        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(data), function (err){
-            if (err) {
-                return console.log(err);
-            }
-             res.send(data);
-            })
-        })
+//delete from database 
+app.delete("/api/notes/:id", function(req, res){
+
 });
-    
-app.delete("/api/notes/:note", function (req, res) {
-    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function (err, data) {
-        if (err) {
-            return console.log(err);
-        } 
-        let remove = req.param.note;
-        data = JSON.parse(data)
-        let result = data.filter( function(data){
-            data.title !== remove;
-            fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(result), function(err){
-                if (err) {
-                    return console.log(err);
-                }
-            }) 
-            res.send(data);
-        });
-    });
-})
+
 
 app.listen(PORT, function () {
     console.log("You did it!" + PORT)
